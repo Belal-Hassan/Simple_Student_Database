6ace958be279efcce257971bc509037c60e81746
Belal Hassan <belal.34327@gmail.com>
2023-11-28 10:58:13 +0200

    Comments added to SDBAPP.h

--- SDBAPP.h
+++ SDBAPP.h
@@ -1,5 +1,5 @@
 #pragma once
 #include "SDB.h"
-
-void SDB_Action(uint8 choice);
-void SDB_APP();
+// This is the header file of the functions used in the main source file.
+void SDB_Action(uint8 choice); // Function for choosing the command to excute in the database.
+void SDB_APP(); // The only function used in the main source file.
